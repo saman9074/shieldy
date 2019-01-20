3ee8bb0fee1a14a45e9002d5b3dd941a449ce995
saman9074 <saman9074@yahoo.com>
2019-01-20 04:23:31 +0330

    add proxy
    add proxy

--- src/helpers/bot.ts
+++ src/helpers/bot.ts
@@ -1,9 +1,19 @@
 import { ContextMessageUpdate, Telegraf } from 'telegraf'
 
+//socks5
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const socksAgent = new SocksAgent({
+  socksHost: process.env.host,
+  socksPort: process.env.port,
+  socksUsername: process.env.login,
+  socksPassword: process.env.pass,
+});
+
+
 // Dependencies
 const TelegrafBot = require('telegraf')
 
-export const bot = new TelegrafBot(process.env.TOKEN) as Telegraf<
+export const bot = new TelegrafBot(process.env.TOKEN, {telegram: { agent: socksAgent }}) as Telegraf<
   ContextMessageUpdate
 >
 
